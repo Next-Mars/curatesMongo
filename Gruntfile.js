eea7d2e4c8042ff96124b27d265d72c22af78083
Daniel Streit <Daniel.Streit@gmail.com>
2014-09-03 17:10:32 -0700

    Adds usual helper program files

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec'
+				},
+				src: ['test/**/*.js']
+			}
+		},
+
+		nodemon: {
+      dev: {
+      	script: 'server.js'
+      }
+		},
+
+		jshint: {
+			files: [
+
+			  'Gruntfile.js',
+			  'client/**/*.js',
+			  'server/**/*.js'
+			],
+			options: {
+				force: 'true',
+				jshintrc: '.jshintrc'
+			}
+		},
+
+		watch: {
+			scripts: {
+        ignores: [
+          'client/lib/**/*.js'
+        ],
+				files: [
+				  'client/**/*.js'
+				],
+				tasks: [
+				  'mochaTest',
+				  'jshint'
+				]
+			}
+		}
+
+	});
+  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+
+  grunt.registerTask('dev', function(target) {
+    var nodemon = grunt.util.spawn({
+    	cmd: 'grunt',
+    	grunt: true,
+    	args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+    grunt.task.run(['watch']);
+
+  });
+
+
+  grunt.registerTask('test', [
+  	  'jshint',
+  	  'mochaTest'
+  	]
+  );
+
+
+};
